1d53914f9121c1eee586fbe97b8cef59acdca383
Bruno Romeiro <bruno.romeiro@gmail.com>
2021-09-02 11:38:09 -0300

    feat: adiciona novos retornos

--- src/custom/db/custom.data.ts
+++ src/custom/db/custom.data.ts
@@ -1,11 +1,17 @@
 import { Customs } from '../custom.interface';
 import { externalDashboardTile } from './dashtile.data';
 import { dataForm } from './data-form';
+import { newsData } from './news-api.data';
 import { podsButton } from './pods-button.data';
 import { podsCalculator } from './pods-calculator.data';
 import { podsInputAnime } from './pods-input-anime.data';
 import { podsInputCNPJ } from './pods-input.data';
 import { xcomp } from './xcomp.data';
+import { pocData } from './poc.data';
+import { pocCNPJ } from './poc-cnpj';
+import { pocCPF } from './poc-cpf';
+import { pocTable } from './poc-table.data';
+import { pocChart } from './poc-chart';
 
 export const customs: Customs = [
   {
@@ -35,5 +41,29 @@ export const customs: Customs = [
   {
     component: 'data-form',
     src: dataForm
+  },
+  {
+    component: 'app-hero',
+    src: newsData
+  },
+  {
+    component: 'hc-root',
+    src: pocData
+  },
+  {
+    component: 'app-cnpj',
+    src: pocCNPJ
+  },
+  {
+    component: 'poc-cpf',
+    src: pocCPF
+  },
+  {
+    component: 'fp-root',
+    src: pocTable
+  },
+  {
+    component: 'chart-root',
+    src: pocChart
   }
 ];
